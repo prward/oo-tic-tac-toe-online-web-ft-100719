467e8f1792c4aa602943893bff4a1fa8536e51dd
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-16 16:52:57 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,133 @@
+class TicTacToe
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+ 
+ WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6]
+    ]
+    
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(position, mark)
+    @board[position] = mark
+  end
+  
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+  
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index)
+  end
+  
+  def turn_count  
+    @board.count{|mark| mark == "X" || mark == "O"}
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index= input_to_index(input)
+    mark = current_player
+    if valid_move?(index)
+      move(index, mark)
+      display_board
+    else 
+      turn
+    end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    else
+      false
+    end
+  end
+end
+   
+   def full?
+    !@board.any?{|x| x == "" || x == " "}
+  end
+  
+  def draw?
+    !(won?) && (full?) 
+  end
+  
+  def over?
+    won? || draw? || full?
+  end
+  
+  def winner
+    if won? 
+      @board[won?[0]] == "X" ? "X" : "O"
+    else 
+      nil
+    end
+  end
+  
+  def play 
+    while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  end
+  if draw?
+    puts "Cat's Game!"
+  end
+end
+end
+
+TicTacToe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
